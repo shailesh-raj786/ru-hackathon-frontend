bb53e3954b6a68f225179e1f278969efb8711189
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-03-20 01:36:41 +0530

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import LandingPage from "./pages/LandingPage";
+import LocalMarket from "./pages/LocalMarket";
+import PostPage from "./pages/PostPage";
+import PricesPage from "./pages/PricesPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/prices" element={<PricesPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/market" element={<LocalMarket />} />
+      </Routes>
+      <Routes>
+        <Route path="/posts" element={<PostPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LocalMarket.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LocalMarket() {
+  return (
+    <div>LocalMarket</div>
+  )
+}
+
+export default LocalMarket
