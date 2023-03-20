d3d2d21482f9bf7311500c7e3614f33b9cf38683
RAM MODI <rammodi0509@gmail.com>
2023-03-20 10:51:49 +0530

    landing update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Upper3 from '../Upper3'
+import Upper4 from '../Upper4'
 import "./pages.css"
 
 function LandingPage() {
   return (
     <div className='landingpage'>
       <Upper3 />
+      <Upper4 />
     </div>
   )
 }
