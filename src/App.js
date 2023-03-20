fcd533294f1a186623efd5c48a80ef82ffed2946
Shailesh Purohit <shailesh432004@gmail.com>
2023-03-20 07:58:52 +0530

    blogs update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import BlogPage from "./pages/BlogPage";
 import LandingPage from "./pages/LandingPage";
 import LocalMarket from "./pages/LocalMarket";
 import PostPage from "./pages/PostPage";
 import PricesPage from "./pages/PricesPage";
+
 
 function App() {
   return (
@@ -22,6 +24,9 @@ function App() {
       </Routes>
       <Routes>
         <Route path="/posts" element={<PostPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/blogs" element={<BlogPage />} />
       </Routes>
     </BrowserRouter>
   );
